5c791453b28dc17567c6efd40695371fecd9197a
Olga Kharabet <kharabet.olga@gmail.com>
2021-10-27 12:15:39 +0000

    Added tables for roles & permissions system

---
+++ database/migrations/2021_10_27_112911_create_roles_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->unsignedBigInteger('role_id');
+			$table->unsignedBigInteger('user_id');
+
+			$table->foreign('role_id')
+				->references('id')
+				->on('roles')
+				->onUpdate('cascade')
+				->onDelete('cascade');
+
+			$table->foreign('user_id')
+				->references('id')
+				->on('users')
+				->onUpdate('cascade')
+				->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles_users');
+    }
+}
