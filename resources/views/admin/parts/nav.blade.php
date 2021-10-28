7228f3c75f37bcd439795041be86267aa98c7c76
Olga Kharabet <kharabet.olga@gmail.com>
2021-10-28 18:36:15 +0000

    Added routes, empty controllers

--- resources/views/admin/parts/nav.blade.php
+++ resources/views/admin/parts/nav.blade.php
@@ -1,20 +1,42 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-	<div class="container">
-		<a class="navbar-brand" href="{{ url('/') }}">
-			{{ config('app.name', 'Laravel') }}
+<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="{{ route('main_page') }}">
+			{{ __('The Example App') }}
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<!-- Left Side Of Navbar -->
-			<ul class="navbar-nav mr-auto">
+		<div class="collapse navbar-collapse" id="navbarScroll">
+			<ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="{{ route('admin.dashboard') }}">Dashboard</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">{{ __('Users Management') }}</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">{{ __('Teams Management') }}</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">{{ __('Orders Management') }}</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						{{ __('Settings') }}
+					</a>
+					<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+						<li><a class="dropdown-item" href="#">Roles</a></li>
+						<li><a class="dropdown-item" href="#">Permissions</a></li>
+						<li><hr class="dropdown-divider"></li>
+						<li><a class="dropdown-item" href="#">Something else here</a></li>
+					</ul>
+				</li>
 
 			</ul>
 
 			<!-- Right Side Of Navbar -->
-			<ul class="navbar-nav ml-auto">
+			<ul class="navbar-nav ml-auto d-flex">
 				<!-- Authentication Links -->
 				@guest
 					@if (Route::has('login'))
@@ -22,22 +44,16 @@
 							<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
 						</li>
 					@endif
-
-					@if (Route::has('register'))
-						<li class="nav-item">
-							<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-						</li>
-					@endif
 				@else
-					<li class="nav-item dropdown">
+					<li class="nav-item dropdown d-flex">
 						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-							{{ Auth::user()->name }}
+							{{ Auth::user()->nickname }}
 						</a>
 
 						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 							<a class="dropdown-item" href="{{ route('logout') }}"
 							   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                               document.getElementById('logout-form').submit();">
 								{{ __('Logout') }}
 							</a>
 
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +21,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('show-login-form');
 Route::post('login', [LoginController::class, 'authenticate'])->name('authenticate');
 
-Route::get('/', function () {
-	return view('admin.dashboard');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
+	Route::get('/', [UserController::class, 'index'])->name('index');
+	Route::get('data', [UserController::class, 'data'])->name('data');
+
+	Route::get('create', [UserController::class, 'create'])->name('create');
+	Route::post('create', [UserController::class, 'store'])->name('store');
+
+	Route::get('{user}', [UserController::class, 'show'])->name('show');
+
+	Route::get('{user}/edit', [UserController::class, 'edit'])->name('edit');
+	Route::post('{user}/edit', [UserController::class, 'update'])->name('update');
+
+	Route::delete('{user}', [UserController::class, 'destroy'])->name('destroy');
 });
 
+Route::group(['prefix' => 'roles', 'as' => 'roles.'], function () {
+	Route::get('/', [RoleController::class, 'index'])->name('index');
+	Route::get('data', [RoleController::class, 'data'])->name('data');
 
-//require __DIR__.'/auth.php';
+	Route::post('{role}', [RoleController::class, 'restore'])->name('restore');
+	Route::delete('{role}', [RoleController::class, 'destroy'])->name('destroy');
+});
+
+Route::group(['prefix' => 'permissions', 'as' => 'permissions.'], function () {
+	Route::get('/', [PermissionController::class, 'index'])->name('index');
+	Route::get('data', [PermissionController::class, 'data'])->name('data');
+
+	Route::post('{permission}', [PermissionController::class, 'restore'])->name('restore');
+	Route::delete('{permission}', [PermissionController::class, 'destroy'])->name('destroy');
+});
