5f7d722fd553061ba9a60bfd51ae61ecd4e9c191
Olga Kharabet <kharabet.olga@gmail.com>
2021-10-29 08:44:57 +0000

    Added resources, collections

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Membership;
+use App\Models\Team;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+		foreach ($this->roles as $role) {
+			$roles[] = $role->name;
+		}
+
+		return [
+			'id' => $this->id,
+			'first_name' => $this->first_name,
+			'last_name' => $this->last_name,
+			'nickname' => $this->nickname,
+			'email' => $this->email,
+			'roles' => $roles,
+			'team' => Team::where('id', $this->current_team_id)->first()->name,
+			'team_membership' => Membership::where('user_id', $this->id)->first()->role,
+		];
+    }
+}
